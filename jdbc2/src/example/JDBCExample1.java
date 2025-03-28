abed712eca9c654bceb64e682a726ec8531052a6
tjseorud <35774943+tjseorud@users.noreply.github.com>
2025-03-28 17:23:00 +0900

    Update ip

--- jdbc2/src/example/JDBCExample1.java
+++ jdbc2/src/example/JDBCExample1.java
@@ -23,7 +23,7 @@ public class JDBCExample1 {
 			/*2. DriverManager 객체를 이용해 Connection 객체생성*/
 			Class.forName("oracle.jdbc.OracleDriver");
 			//jdbc 드라이버가 어떤 데이터베이스에 연결할지 지정
-			String url ="jdbc:oracle:thin:@127.0.0.1:12345:XE";	//학원 DB 서버 URL
+			String url ="jdbc:oracle:thin:@192.168.130.17:1521:XE";	//학원 DB 서버 URL
 			String userName ="KH14_SDK";	//사용자 계정명
 			String password ="KH1234";		//계정 비밀번호
 			
